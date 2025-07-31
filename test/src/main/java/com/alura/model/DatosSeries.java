54d2ce0459d9286fa318ad2bd50760c00efc4a00
David <ambrosiojosedavid@gmail.com>
2025-07-30 23:25:32 -0600

    feat: estructura para mapear objetos de tipo Series

---
+++ test/src/main/java/com/alura/model/DatosSeries.java
@@ -0,0 +1,12 @@
+package com.alura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosSeries(
+        @JsonAlias("Title")  String titulo, 
+        @JsonAlias("totalSeasons") Integer totalTemporadas, 
+        @JsonAlias("imdbRating") String evaluacion) {
+    
+}

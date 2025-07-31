8e575d401fbf18a1e638f6fb65c891d23134c9a8
David <ambrosiojosedavid@gmail.com>
2025-07-30 23:27:55 -0600

    feat: consumo de API OMB (series, peliculas)

--- test/src/main/java/com/alura/controller/Principal.java
+++ test/src/main/java/com/alura/controller/Principal.java
@@ -1,8 +1,12 @@
 package com.alura.controller;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alura.model.DatosSeries;
+import com.alura.services.ConsumoAPI;
+import com.alura.services.ConvierteDatos;
 
 
 @SpringBootApplication
@@ -13,8 +17,13 @@ public class Principal implements CommandLineRunner{
 
     @Override //metodo que se ejecutara una única vez al iniciar el programa
     public void run(String... args) throws Exception { 
-        System.out.println("Hola mundo !!");
- 
+        ConsumoAPI consumoApi = new ConsumoAPI();
+
+        String json = consumoApi.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&apikey=84b7b6d1&type");
+        ConvierteDatos convierteD = new ConvierteDatos();
+        DatosSeries datosS = convierteD.obtenerDatos(json, DatosSeries.class);
+        System.err.println(datosS.toString());
+        
     }
 
 }

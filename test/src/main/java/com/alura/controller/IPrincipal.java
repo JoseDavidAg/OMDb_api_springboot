a7c8f60817222377df59492819a37e6d325907cc
David <ambrosiojosedavid@gmail.com>
2025-08-01 00:11:46 -0600

    feat: mejorar la visualización de episodios y convertir datos a objetos Episodio. Usar Streams para mejorar el resultado trabajando con listas.

--- test/src/main/java/com/alura/controller/IPrincipal.java
+++ test/src/main/java/com/alura/controller/IPrincipal.java
@@ -1,16 +1,20 @@
 package com.alura.controller;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
+import com.alura.model.DataEpisodio;
 import com.alura.model.DataTemporadas;
 import com.alura.model.DatosSeries;
+import com.alura.model.Episodio;
 import com.alura.services.ConsumoAPI;
 import com.alura.services.ConvierteDatos;
 
 public class IPrincipal {
-    private Scanner sc = new Scanner(System.in);
+    private final Scanner sc = new Scanner(System.in);
     private final String URL_A = "http://www.omdbapi.com/?";
     private final String API_K = "&apikey=84b7b6d1";
     ConsumoAPI consumoApi = new ConsumoAPI();
@@ -45,7 +49,25 @@ public class IPrincipal {
             }
         }*/
         
-        temporadas.forEach(t-> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        //Imprime el nombre de todos los episodios
+        //temporadas.forEach(t-> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        //Convertir toda la información en objetos de tipo DataEpisodio -> ver serie, ver temporadas, ver todos los episodios
+
+        List<DataEpisodio> episodios = temporadas.stream()  
+            .flatMap(t -> t.episodios().stream())
+            .toList();
+
+        //top 5 episodios
+        System.out.println("Top 5 episodios de la serie:");
+        episodios.stream().filter(m -> !m.evaluacion().equalsIgnoreCase("N/A")).sorted(Comparator.comparing(DataEpisodio::evaluacion).reversed()).limit(5).forEach(System.out::println);
+
+        //Convertir los datos de tipo Episodio
+        List<Episodio> episodiosM =  temporadas.stream()   
+            .flatMap(t -> t.episodios().stream()
+                .map(d -> new Episodio(t.numTemporada(), d))).collect(Collectors.toList());
+            
+        episodiosM.forEach(System.out::println);
         
     }
 

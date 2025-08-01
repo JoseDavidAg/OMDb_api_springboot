e990f8c33273e254c9aff65a1191bcfbfd26927e
David <ambrosiojosedavid@gmail.com>
2025-07-31 22:53:04 -0600

    refactor: eliminar importaciones no utilizadas y simplificar el método run en la clase Principal

--- test/src/main/java/com/alura/controller/Principal.java
+++ test/src/main/java/com/alura/controller/Principal.java
@@ -4,10 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.alura.model.DatosSeries;
-import com.alura.services.ConsumoAPI;
-import com.alura.services.ConvierteDatos;
-
 
 @SpringBootApplication
 public class Principal implements CommandLineRunner{
@@ -17,12 +13,12 @@ public class Principal implements CommandLineRunner{
 
     @Override //metodo que se ejecutara una única vez al iniciar el programa
     public void run(String... args) throws Exception { 
-        ConsumoAPI consumoApi = new ConsumoAPI();
+        IPrincipal principal = new IPrincipal();
+        principal.mostrarMenu();
+    
+        
+       
 
-        String json = consumoApi.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&apikey=84b7b6d1&type");
-        ConvierteDatos convierteD = new ConvierteDatos();
-        DatosSeries datosS = convierteD.obtenerDatos(json, DatosSeries.class);
-        System.err.println(datosS.toString());
         
     }
 

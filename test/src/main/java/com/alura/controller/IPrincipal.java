28aa8729659f80981366f8ad4e609cd9aa16d52b
David <ambrosiojosedavid@gmail.com>
2025-08-06 14:01:07 -0600

    feat: mejorar la búsqueda de episodios y agregar opción para buscar serie por nombre

--- test/src/main/java/com/alura/controller/IPrincipal.java
+++ test/src/main/java/com/alura/controller/IPrincipal.java
@@ -3,11 +3,13 @@ package com.alura.controller;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import com.alura.model.DataTemporadas;
 import com.alura.model.DatosSeries;
+import com.alura.model.Episodio;
 import com.alura.model.Serie;
 import com.alura.repository.SerieRepository;
 import com.alura.services.ConsumoAPI;
@@ -17,23 +19,24 @@ public class IPrincipal {
     private final Scanner teclado = new Scanner(System.in);
     private final ConsumoAPI consumoApi = new ConsumoAPI();
     private final String URL_BASE = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "&apikey=84b7b6d1";
-    private final ConvierteDatos conversor = new ConvierteDatos();
+    private final String API_KEY = "&apikey="+System.getenv("DB_OMDBAPI");
+    private ConvierteDatos conversor = new ConvierteDatos();
     private List<DatosSeries> datosSeries = new ArrayList<>();
-    private List<Serie> datosSerieObjeto = new ArrayList<>();
-    private SerieRepository repositorioSerie;
-    public IPrincipal(SerieRepository serie) {
-        repositorioSerie = serie;
+    private SerieRepository repositorio;
+    private List<Serie> series;
+
+    public IPrincipal(SerieRepository repository) {
+        this.repositorio = repository;
     }
 
     public void muestraElMenu() {
-        int opcion = -1;
+        var opcion = -1;
         while (opcion != 0) {
-            String menu = """
+            var menu = """
                     1 - Buscar series 
                     2 - Buscar episodios
                     3 - Mostrar series buscadas
-                                  
+                    4 - Buscar serie por nombre              
                     0 - Salir
                     """;
             System.out.println(menu);
@@ -48,8 +51,10 @@ public class IPrincipal {
                     buscarEpisodioPorSerie();
                     break;
                 case 3:
-                    mostarSeriesBuscadas();
-
+                    mostrarSeriesBuscadas();
+                    break;
+                case 4: 
+                    buscarSeriePorNombre();
                 case 0:
                     System.out.println("Cerrando la aplicación...");
                     break;
@@ -64,39 +69,72 @@ public class IPrincipal {
         System.out.println("Escribe el nombre de la serie que deseas buscar");
         var nombreSerie = teclado.nextLine();
         var json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + API_KEY);
-        //System.out.println(json);
+        System.out.println(json);
         DatosSeries datos = conversor.obtenerDatos(json, DatosSeries.class);
         return datos;
     }
+    
     private void buscarEpisodioPorSerie() {
-        DatosSeries datosSerie = getDatosSerie();
-        List<DataTemporadas> temporadas = new ArrayList<>();
+        mostrarSeriesBuscadas();
+        System.out.println("Escribe el nombre de la seria de la cual quieres ver los episodios");
+        var nombreSerie = teclado.nextLine();
 
-        for (int i = 1; i <= datosSerie.totalTemporadas(); i++) {
-            var json = consumoApi.obtenerDatos(URL_BASE + datosSerie.titulo().replace(" ", "+") + "&season=" + i + API_KEY);
-            DataTemporadas datosTemporada = conversor.obtenerDatos(json, DataTemporadas.class);
-            temporadas.add(datosTemporada);
+        Optional<Serie> serie = series.stream()
+                .filter(s -> s.getTitulo().toLowerCase().contains(nombreSerie.toLowerCase()))
+                .findFirst();
+
+        if(serie.isPresent()){
+            var serieEncontrada = serie.get();
+            List<DataTemporadas> temporadas = new ArrayList<>();
+
+            for (int i = 1; i <= serieEncontrada.getTotalTemporadas(); i++) {
+                var json = consumoApi.obtenerDatos(URL_BASE + serieEncontrada.getTitulo().replace(" ", "+") + "&season=" + i + API_KEY);
+                DataTemporadas datosTemporada = conversor.obtenerDatos(json, DataTemporadas.class);
+                temporadas.add(datosTemporada);
+            }
+            temporadas.forEach(System.out::println);
+
+            List<Episodio> episodios = temporadas.stream()
+                    .flatMap(d -> d.episodios().stream()
+                            .map(e -> new Episodio(d.numTemporada(), e)))
+                    .collect(Collectors.toList());
+
+            serieEncontrada.setEpisodios(episodios);
+            repositorio.save(serieEncontrada);
         }
-        temporadas.forEach(System.out::println);
+
+
+
     }
+
+
+
+    private void mostrarSeriesBuscadas() {
+        series = repositorio.findAll();
+
+        series.stream()
+                .sorted(Comparator.comparing(Serie::getGenero))
+                .forEach(System.out::println);
+    }
+
     private void buscarSerieWeb() {
         DatosSeries datos = getDatosSerie();
-        datosSeries.add(datos);
+        Serie serie = new Serie(datos);
+        repositorio.save(serie);
+        //datosSeries.add(datos);
         System.out.println(datos);
     }
 
-    private void mostarSeriesBuscadas(){
+    private void buscarSeriePorNombre(){
+        System.out.println("Escriba el nombre de la serie que desea buscar: ");
+        String nombreSerie = teclado.nextLine().toUpperCase();
+        Optional<Serie> serieBuscar = repositorio.findByTituloContainsIgnoreCase(nombreSerie);
+        if(serieBuscar.isPresent()){
+            System.out.println("La serie encontrada es :"+serieBuscar.get());
+        }else{
+            System.out.println("Serie no encontrada!");
+        }
 
-        // datosSeries.forEach(t-> datosSerieObjeto.add(new Serie(t)));
-        // datosSerieObjeto.forEach(System.out::println);
-
-        datosSerieObjeto = datosSeries.stream()
-            .map(t-> new Serie(t)).collect(Collectors.toList());
-            
-        datosSerieObjeto.stream()
-            .sorted(Comparator.comparing(Serie::getGenero)).forEach(System.out::println);
 
     }
-
-
 }

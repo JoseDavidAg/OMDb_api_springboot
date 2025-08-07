fe4be42438c277c7d721bbf6b8921c3d1f54228a
David <ambrosiojosedavid@gmail.com>
2025-08-06 18:05:01 -0600

    feat: agregar categoría en español a la enumeración Categoria y actualizar el constructor

--- test/src/main/java/com/alura/model/Categoria.java
+++ test/src/main/java/com/alura/model/Categoria.java
@@ -2,23 +2,34 @@ package com.alura.model;
 
 
 public enum Categoria{
-    ACCION("Action"),
-    ROMANCE("Romance"),
-    DRAMA("Drama"),
-    CRIMEN("Crime"),
-    COMEDIA("Comedy"),
-    HORROR("Horror");
+    ACCION("Action", "Acción"),
+    ROMANCE("Romance", "Romance"),
+    DRAMA("Drama","Drama"),
+    CRIMEN("Crime","Crimen"),
+    COMEDIA("Comedy","Comedia"),
+    HORROR("Horror","Horror");
 
     String categoriaOmdb;
+    String categoriaEspanol; 
 
-    Categoria(String categoriaOmdb){
+    Categoria(String categoriaOmdb, String categoriaEspanol){
         this.categoriaOmdb = categoriaOmdb;
+        this.categoriaEspanol= categoriaEspanol;
     }
 
 
     public static Categoria fromString(String text) {
         for(Categoria categoria : Categoria.values()){
             if(categoria.categoriaOmdb.equalsIgnoreCase(text)){
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada "+text);
+    }
+
+     public static Categoria fromCategoriaEspanol(String text) {
+        for(Categoria categoria : Categoria.values()){
+            if(categoria.categoriaEspanol.equalsIgnoreCase(text)){
                 return categoria;
             }
         }

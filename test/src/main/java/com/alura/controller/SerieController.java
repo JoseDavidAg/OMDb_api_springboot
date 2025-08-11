c4deed0e8bd63a4014b1e15e14726de1b6e32928
David <ambrosiojosedavid@gmail.com>
2025-08-11 00:56:00 -0600

    feat: rutas de respuesta del back-end disponibles usando objetos Dto

--- test/src/main/java/com/alura/controller/SerieController.java
+++ test/src/main/java/com/alura/controller/SerieController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alura.dto.EpisodioDto;
 import com.alura.dto.SerieDto;
 import com.alura.services.SerieService;
 
@@ -18,11 +20,43 @@ public class SerieController {
     
     //para obtener un respuesta ante localhost:5080/series
     //DTO para proteger datos 
-    
+
     @GetMapping("/series")
     public List<SerieDto> mostrarMensaje(){
-        return service.mostrarMensaje();
+        return service.obtenerTodasSeries();
     }
+
+    @GetMapping("/series/top5")
+    public List<SerieDto> obtenerTop5Series(){
+        return service.obtenerTopSeries();
+    }
+
+    @GetMapping("/series/lanzamientos")
+    public List<SerieDto> obtenerLanzamientos(){
+        return service.obtenerSeriesRecientes();
+    }
+
+    @GetMapping("series/{id}")
+    public SerieDto obtenerSerieId(@PathVariable Long id){
+        return service.obtenerId(id);
+    }
+
+    @GetMapping("series/{id}/temporadas/todas")
+    public List<EpisodioDto> obtenerEpisodios(@PathVariable Long id){
+        return service.obtenerTodosEpisodios(id);
+
+    }
+
+     @GetMapping("/series/{id}/temporadas/{numeroTemporada}")
+     public List<EpisodioDto> obtenerEpisodiosTemporada(@PathVariable Long id, @PathVariable Long numeroTemporada){
+        return service.obtenerEpisodioPorTemporada(id, numeroTemporada);
+    } 
+
+    @GetMapping("/series/categoria/{categoriaSeleccionada}")
+    public List<SerieDto> obtenerSeriePorCategoria(@PathVariable String categoriaSeleccionada){
+        return service.obtenerSerieCategoria(categoriaSeleccionada);
+    }
+    
 
 
 }
